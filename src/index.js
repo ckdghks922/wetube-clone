57439bdce7afe746de6d11dcfc6bb6b1ef736544
ckdghks922 <ch.park@kaist.ac.kr>
2021-08-09 16:33:52 +0900

    feat: add sample middleware

--- src/index.js
+++ src/index.js
@@ -3,14 +3,20 @@ import express from "express";
 const PORT = 4000;
 const app = express();
 
+const gossipMiddleWare = (req, res, next) => {
+  console.log(`Someone is going to: ${req.url}`);
+  next();
+};
+
 const handleGoHome = (req, res) => {
-  return res.send("send");
+  return res.send("I love MiddleWares!");
 };
 
 const handleLogin = (req, res) => {
   return res.send("Login here.");
 };
 
+app.use(gossipMiddleWare);
 app.get("/", handleGoHome);
 app.get("/login", handleLogin);
 
