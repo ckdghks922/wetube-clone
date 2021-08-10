2b832df9a2962babcd476a7e1054102537d33d31
changhwan_park <changhwan_park@tmax.co.kr>
2021-08-11 00:00:08 +0900

    chore: add request method console

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import express from "express";
 const PORT = 4000;
 const app = express();
 
-const gossipMiddleWare = (req, res, next) => {
-  console.log(`Someone is going to: ${req.url}`);
+const logger = (req, res, next) => {
+  console.log(`${req.method}: ${req.url}`);
   next();
 };
 
@@ -12,13 +12,7 @@ const handleGoHome = (req, res) => {
   return res.send("I love MiddleWares!");
 };
 
-const handleLogin = (req, res) => {
-  return res.send("Login here.");
-};
-
-app.use(gossipMiddleWare);
-app.get("/", handleGoHome);
-app.get("/login", handleLogin);
+app.get("/", logger, handleGoHome);
 
 const handleListening = () => {
   console.log(`Server Listening on port http://localhost:${PORT}`);
